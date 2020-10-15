f23fcd1bb7e08d0ab9d4a4be0cfef251f7fb04dd
Dennis HG <dennishg32@gmail.com>
2020-10-15 09:49:25 +0200

    addition of scroll function and change logo

--- js/index.js
+++ js/index.js
@@ -7,9 +7,46 @@ function CyeshaInfo(fullname, frequency, location, email, phonenumber) {
     this.email = email;
 }
 
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+    );
+}
 
 
+//interface logic
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 200) {
+        $('.fixed-top').css({ 'background-color': "#2EB82E", 'box-shadow': ' box-shadow: 0 3px 1rem rgba(0,0,0,.5)' });
+        $('.navbar-light .navbar-nav .nav-link ').css('color', 'white');
+        // $('#phone .nav-link').css('color','#2EB82E');
+        // $('.navbar-light .navbar-nav .nav-link::before ').css({'content': "''", 'position': 'absolute','width':' 100%','transform':'scale(0)',
+        // 'height':' 2px','bottom': '0','left': '0','transform-origin':'bottom right','background': '#white', 'transition':'transform 0.5s ease-out'});
+        // $('.navbar-light .navbar-nav .nav-link:hover::before').css({' transform': 'scale(1)','transform-origin':'bottom left' });
+    } else {
+        $('.fixed-top').css('background-color', "transparent");
+        $('.navbar-light .navbar-nav .nav-link ').css('color', '#fff');
+        $('#phone .nav-link').css('color', 'white');
+    }
+})
 
+$("form#myForm").submit(function(event) {
+    var name = $("input#nameTxt").val();
+    var email = $("input#mailTxt").val();
+    var msg = $("textarea#messageTxt").val();
+
+    if (name != "" && email != "" && msg != "") {
+        alert(
+            name +
+            ", We have recieved your feedback. Thank you for your support and reach to us, it means a lot"
+        );
+    } else if (!isEmail(email)) {
+        alert("incomplete, try again please");
+    } else {
+        alert("invalid input");
+    }
+    event.preventDefault();
+});
 $("form#carwash").submit(function(event) {
     event.preventDefault();
     var inputFname = $("input#fullname").val();
@@ -55,30 +92,4 @@ $("form#officeform").submit(function(event) {
     // $(".location-request").text(inputLocation);
     // $(".frequency-request").text(inputFrequency);
     // console.log(inputSizeOffice);
-});
-
-
-function isEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        email
-    );
-}
-
-//interface logic
-$("form#myForm").submit(function(event) {
-    var name = $("input#nameTxt").val();
-    var email = $("input#mailTxt").val();
-    var msg = $("textarea#messageTxt").val();
-
-    if (name != "" && email != "" && msg != "") {
-        alert(
-            name +
-            ", We have recieved your feedback. Thank you for your support and reach to us, it means a lot"
-        );
-    } else if (!isEmail(email)) {
-        alert("incomplete, try again please");
-    } else {
-        alert("invalid input");
-    }
-    event.preventDefault();
 });
